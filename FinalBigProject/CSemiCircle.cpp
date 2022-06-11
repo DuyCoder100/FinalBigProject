d1ed4de0f3a28d655d5c4a2c8f8d06b13e3e0283
DuyCoder100 <75831935+DuyCoder100@users.noreply.github.com>
2022-06-11 08:07:24 +0700

    Project completed!

--- FinalBigProject/CSemiCircle.cpp
+++ FinalBigProject/CSemiCircle.cpp
@@ -17,10 +17,9 @@ void CSemiCircle::Draw(HDC hdc, Point LeftTop, Point RightBottom)
 
 	int radius = (RightBottom.x - LeftTop.x) / 2;
 	BeginPath(hdc);
-	MoveToEx(hdc, LeftTop.x + radius, LeftTop.y, 0);
-	LineTo(hdc, LeftTop.x, LeftTop.y);
-	AngleArc(hdc, LeftTop.x, LeftTop.y, radius, 0, 180);
-	LineTo(hdc, LeftTop.x + radius, LeftTop.y);
+	MoveToEx(hdc, LeftTop.x, RightBottom.y, 0);
+	AngleArc(hdc, LeftTop.x, RightBottom.y, radius, 0, 180);
+	LineTo(hdc, LeftTop.x, RightBottom.y);
 	EndPath(hdc);
 	StrokeAndFillPath(hdc);
 
@@ -81,9 +80,10 @@ bool CSemiCircle::isIntersect(Line l)
 	vector<int> coefs = l.getCoefs();
 	double a = coefs[0] * 1.0;
 	double b = coefs[1] * 1.0;
-	double c = -coefs[2] * 1.0;
+	double c = coefs[2] * 1.0;
 
-
+	double m = -a / b;
+	double n = c / b;
 	//E: (x - xO)^2 + (y - yO)^2 = R^2
 	double R = getRadius() * 1.0;
 
@@ -99,13 +99,45 @@ bool CSemiCircle::isIntersect(Line l)
 	N.x = this->m_RightBottom.x;
 	N.y = this->m_RightBottom.y;
 
-	Line MN;
-	MN.setToaDo(M, N);
+	double A = m * m + 1;
+	double B = 2 * (m * (n - yO) - xO);
+	double C = xO * xO + (n - yO) * (n - yO) - R * R;
 
-	double d = abs(a * xO + b * yO + c) / sqrt(a * a + b * b);
-
-	return (isOutsideSemiCircle(l.A) && isOutsideSemiCircle(l.B)
-		&& d < R && (l.A.y >= yO || l.B.y >= yO));
+	double delta = B * B - 4 * A * C;
+	if (delta >= 0)
+	{
+		double x1 = (-b - sqrt(delta)) / (2 * a);
+		double x2 = (-b + sqrt(delta)) / (2 * a);
+		if (M.x < N.x)
+		{
+			if (((m * x1 + n) < yO) || (m * x2 + n) < yO)
+			{
+				return true;
+			}
+		}
+		if (M.x > N.x)
+		{
+			if (((m * x1 + n) > yO) || (m * x2 + n) > yO)
+			{
+				return true;
+			}
+		}
+		if (M.y < N.y)
+		{
+			if (x1 > xO || x2 > xO)
+			{
+				return true;
+			}
+		}
+		if (M.y > N.y)
+		{
+			if (x1 < xO || x2 < xO)
+			{
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 string CSemiCircle::getClassName()
